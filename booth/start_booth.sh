0f8c00325b8733a99244471d8141326d3c1c385e
Mick Krippendorf <m.krippendorf@freenet.de>
2017-05-02 01:19:08 +0200

    Script zum Aushängen der Kamera.

--- booth/start_booth.sh
+++ booth/start_booth.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-set -o nounset
-
-gmount=$( gvfs-mount -l  |  sed -n '/gphoto2:/ {s/.* //p; q}' )
-if [ $gmount ]
-then
-    echo "Kamera $gmount wird ausgehängt."
-    gvfs-mount -u  "$gmount"
-    sleep 0.5
-else
-    echo "Kamera ist nicht eingehängt."
-fi
-
 python booth.py
 
 if [ $? -eq 64 ]
